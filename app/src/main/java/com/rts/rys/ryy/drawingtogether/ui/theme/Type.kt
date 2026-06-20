6708e9e744be0c628d2acbd1bedf468360c1e507
ts.Ryu <lts06069@naver.com>
2026-06-20 15:38:23 +0900

    드로잉 기능 - 16
    - 폰트 적용

--- app/src/main/java/com/rts/rys/ryy/drawingtogether/ui/theme/Type.kt
+++ app/src/main/java/com/rts/rys/ryy/drawingtogether/ui/theme/Type.kt
@@ -5,30 +5,31 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.rts.rys.ryy.drawingtogether.R
+
+
+// 2. 사용할 커스텀 폰트 패밀리를 먼저 정의합니다.
+val MyCustomFontFamily = FontFamily(
+    Font(resId = R.font.normal, weight = FontWeight.Normal),
+    Font(resId = R.font.bold, weight = FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MyCustomFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.5.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    bodySmall = TextStyle(
+        fontFamily = MyCustomFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 0.4.sp,
+    ),
+
 )
